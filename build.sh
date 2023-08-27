6f6eda8f35142e0bc9103cb92fe22247956e5d0b
Magnus Ahlstromer V <mahlstromerv@gmail.com>
2023-08-27 15:47:58 -0700

    Always configure project when compiling

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd build
 
 if [ "$1" == "clean" ]; then
     make clean
-    
+
     echo -n "Would you like to remove config files too? (y/n): "
     read removeConfig
     if [ $removeConfig == 'y' ]; then
@@ -11,28 +11,29 @@ if [ "$1" == "clean" ]; then
         rm Nova/Makefile
         rm Sandbox/Makefile
     fi
-    
-    cd ..
-else
-    echo -n "Would you like to configure your project? (y/n): "
-    read configure
-    if [ $configure == 'y' ]; then
-        if [ "$1" == "debug" ]; then
-            cmake -DGLFW_BUILD_DOCS=OFF -DCMAKE_BUILD_TYPE=Debug ..
-        elif [ "$1" == "release" ]; then
-            cmake -DGLFW_BUILD_DOCS=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-        fi
 
-        mv compile_commands.json ..
+    cd .. 
+elif [ "$1" == "run" ]; then
+    cd Sandbox
+    ./SandboxApp
+    cd ../..
+else
+    if [ "$1" == "debug" ]; then
+        cmake -DGLFW_BUILD_DOCS=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug ..
+    elif [ "$1" == "release" ]; then
+        cmake -DGLFW_BUILD_DOCS=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
     fi
+
+    mv compile_commands.json ..
 
     make all
 
     echo -n "Would you like to run the project? (y/n): "
     read execute
     if [ $execute == 'y' ]; then
-        ./Sandbox/SandboxApp
+        cd Sandbox
+        ./SandboxApp
     fi
 
-    cd ..
+    cd ../..
 fi
