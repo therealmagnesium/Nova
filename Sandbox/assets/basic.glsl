1a65654b9cb0d99449629d0a21bfa4f92b027bc8
Magnus Ahlstromer V <mahlstromerv@gmail.com>
2023-09-03 02:05:14 -0700

    Add assets folder in Sandbox

---
+++ Sandbox/assets/basic.glsl
@@ -0,0 +1,21 @@
+//!type vertex
+#version 330 core
+layout (location = 0) in vec3 aPos;
+uniform mat4 uModel;
+uniform mat4 uView;
+uniform mat4 uProj;
+
+void main()
+{
+    gl_Position = uProj * uView * uModel * vec4(aPos, 1.0);
+}
+
+//!type fragment
+#version 330 core
+out vec4 fragColor;
+uniform vec4 uColor;
+
+void main()
+{
+    fragColor = uColor;
+} 
